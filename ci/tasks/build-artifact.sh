252fcd63c5d8f62241c94a778bbc8e433d4c3df9
Lukas 'Erdk' Redynk <mr.erdk@gmail.com>
2018-01-10 14:53:46 +0100

    pipeline tests

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -2,8 +2,7 @@
 set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-version=`cat version/number`
 cd music-repo
 #echo $version
-gradle assemble -PVersion=$version
+gradle assemble 
 #ls build/libs/
